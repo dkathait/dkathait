9190817d89589cedb0d92cbc74f888cbe8591b0a
dkathait <106729071+dkathait@users.noreply.github.com>
2022-06-04 22:43:06 +0530

    Create 22.py

---
+++ 22.py
@@ -0,0 +1,6 @@
+pa=int(input("enter principle value"))
+ri=int(input("enter interest rate"))
+yr=int(input("enter np of yrs"))
+ 
+ci=(pa*ri*yr)/100
+print("%d is the compound intrest on given data" %ci)
