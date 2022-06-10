4de737640a20191b56e210f5e51daa2dda5dc37c
dkathait <106729071+dkathait@users.noreply.github.com>
2022-06-11 02:43:09 +0530

    Create 83.py

---
+++ 83.py
@@ -0,0 +1,6 @@
+#alphabet or digit
+val=input("enter the value")
+if(val>='a'and val<='z') or (val<='A'and val>='Z'):
+    print("is a alphabet")
+elif(val>='0' or val<='9'):
+    print("it is digit")
