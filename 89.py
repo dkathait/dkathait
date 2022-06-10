5dbdfbab0f65e29600c5c3991b1a711d6e21add7
dkathait <106729071+dkathait@users.noreply.github.com>
2022-06-11 02:46:49 +0530

    Create 89.py

---
+++ 89.py
@@ -0,0 +1,6 @@
+#vowel or consunent
+val=input("enter the val")
+if(val=='a'or val=='e'or val=='i' or val=='o'or val=='u' ) or (val=='A'or val=='E'or val=='I' or val=='O'or val=='U' ): 
+    print("is vowel")
+else:
+    print("is a consunent")
