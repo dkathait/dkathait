d6c0dbeba4bb7c84a5142833689cc9d604a26895
dkathait <106729071+dkathait@users.noreply.github.com>
2022-06-11 02:52:02 +0530

    Create 100.py

---
+++ 100.py
@@ -0,0 +1,8 @@
+#count
+i=input("enter the val")
+v=0
+for val in i:
+
+    if(val=='a'or val=='e'or val=='i' or val=='o'or val=='u' ) or (val=='A'or val=='E'or val=='I' or val=='O'or val=='U' ): 
+        v=v+1
+print(" total of vowel",v)
